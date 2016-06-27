58435d922795136406931eda52d690e6416819b8
cjjujubee <connie.jew@gmail.com>
2016-06-27 10:37:34 -0700

    Updates package.json and routes

--- routes/question.js
+++ routes/question.js
@@ -7,7 +7,7 @@ var app = express();
 
 //TODO: get actual file names when done with models
 var User = require('../model/user');
-var Question = require('../model/question');
+var Question = require('./questions');
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -25,10 +25,10 @@ mongoose.connection.once('open', function() {
   console.log('DB CONNECTION SUCCESSFUL');
 });
 
-app.get('/users', function(req, res) {
-  User.find({}, function(err, users) {
-    res.json('i am a user');
-  })
+app.get('/questions', function(req, res) {
+    Question.find({}, function(err, users) {
+        res.json(questions);
+    });
 });
 
 app.listen(3000, function() {
