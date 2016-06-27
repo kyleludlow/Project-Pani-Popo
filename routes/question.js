8570b8c0134330946eb956466a0f740ad965d648
cjjujubee <connie.jew@gmail.com>
2016-06-27 11:09:47 -0700

    Adds configuration files and services for questions and updates route

--- routes/question.js
+++ routes/question.js
@@ -1,36 +1,15 @@
-require('../db/connect');
-var mongoose = require('mongoose');
-var schema = mongoose.Schema;
-var bodyParser = require('body-parser');
 var express = require('express');
-var app = express();
+var mongoose = require('mongoose');
+var router = express.Router();
+var Question = require('../services/question');
 
 //TODO: get actual file names when done with models
-var User = require('../model/user');
 var Question = require('./questions');
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
-mongoose.connection.on('error', console.error.bind(console, 'CONNECTION ERROR MESSAGE:'));
-mongoose.connection.once('open', function() {
-  console.log('DB CONNECTION SUCCESSFUL');
-});
-
-app.get('/questions', function(req, res) {
+router.get('/questions', function(req, res) {
     Question.find({}, function(err, users) {
         res.json(questions);
     });
 });
 
-app.listen(3000, function() {
-    console.log('Running on port 3000');
-});
+module.exports = router;
