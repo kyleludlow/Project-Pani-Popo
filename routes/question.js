15929e2442492c770aa1863f1134b7abaa727a14
cjjujubee <connie.jew@gmail.com>
2016-06-27 16:25:07 -0700

    Adds ability to seed a question

--- routes/question.js
+++ routes/question.js
@@ -3,12 +3,19 @@ var mongoose = require('mongoose');
 var router = express.Router();
 var Question = require('../services/question');
 
-//TODO: get actual file names when done with models
-var Question = require('./questions');
-
 router.get('/questions', function(req, res) {
-    Question.find({}, function(err, users) {
+    Question.list(function(questions) {
         res.json(questions);
+    }, function(err) {
+        res.status(400).json(err);
+    });
+});
+
+router.post('/questions', function(req, res) {
+    Question.save(req.body.name, function(question) {
+        res.status(201).json(question);
+    }, function(err) {
+        res.status(400).json(err);
     });
 });
 
--- routes/questions.js
+++ routes/questions.js
@@ -1,53 +1,53 @@
 var questions = [
   {
-    text: 'What is the Samoan word for potatoes?',
+    question: 'What is the Samoan word for potatoes?',
     answer: 'pateta',
     options: ['pateta', 'tamato', 'aniani', 'karoti']
   },
   {
-    text: 'What is the Samoan word for tomatoes?',
+    question: 'What is the Samoan word for tomatoes?',
     answer: 'tamato',
     options: ['pateta', 'tamato', 'aniani', 'karoti']
   },
   {
-    text: 'What is the Samoan word for onions?',
+    question: 'What is the Samoan word for onions?',
     answer: 'aniani',
     options: ['pateta', 'tamato', 'aniani', 'karoti']
   },
   {
-    text: 'What is the Samoan word for carrots?',
+    question: 'What is the Samoan word for carrots?',
     answer: 'karoti',
     options: ['pateta', 'tamato', 'aniani', 'karoti']
   },
   {
-    text: 'What is the Samoan word for apples?',
+    question: 'What is the Samoan word for apples?',
     answer: 'apu',
     options: ['apu', 'tamato', 'aniani', 'karoti']
   },
   {
-    text: 'What is the Samoan word for bananas?',
+    question: 'What is the Samoan word for bananas?',
     answer: 'fa\'l',
     options: ['apu', 'fa\'l', 'aniani', 'karoti']
   },
   {
-    text: 'What is the Samoan word for oranges?',
+    question: 'What is the Samoan word for oranges?',
     answer: 'moli',
     options: ['apu', 'fa\'l', 'moli', 'karoti']
 
   },
   {
-    text: 'What is the Samoan word for bread?',
+    question: 'What is the Samoan word for bread?',
     answer: 'falaoa',
     options: ['apu', 'fa\'l', 'moli', 'falaoa']
 
   },
   {
-    text: 'What is the Samoan word for milk?',
+    question: 'What is the Samoan word for milk?',
     answer: 'susu',
     options: ['susu', 'fa\'l', 'moli', 'falaoa']
   },
   {
-    text: 'What is the Samoan word for butter?',
+    question: 'What is the Samoan word for butter?',
     answer: 'pata',
     options: ['susu', 'pata', 'moli', 'falaoa']
   }
