69406e619b6d15741d74062130e328d9a6c803d7
cjjujubee <connie.jew@gmail.com>
2016-06-28 10:57:25 -0700

    Updates question insertion method to avoid async issues

--- db/questions.js
+++ db/questions.js
@@ -50,8 +50,67 @@ var questions = [
     question: 'What is the Samoan word for butter?',
     answers: ['susu', 'pata', 'moli', 'falaoa'],
     correctAnswer: 'pata'
+  },
+  {
+    question: 'Hello',
+    answers: ['Fa\'afetai','Tālofa', 'Tūlou', 'Leai'],
+    correctAnswer: 'Tālofa'
+  },
+  {
+    question: 'Good bye',
+    answers: ['Fa\'afetai','Tālofa', 'Tālofa', 'Fā soifua'],
+    correctAnswer: 'Fā soifua'
+  },
+  {
+    question: 'Yes',
+    answers: ['\'Ioe','Tālofa', 'Malō', 'Leai'],
+    correctAnswer: '\'Ioe'
+  },
+  {
+    question: 'No',
+    answers: ['\'Ioe','Leai', 'Tūlou', 'Malō'],
+    correctAnswer: '\'Ioe'
+  },
+  {
+    question: 'How are you?',
+    answers: ['\'O ā mai \'oe','Fa\'afetai fo\'i.', 'Tōfā soifua.', 'Manuia le taeao'],
+    correctAnswer: '\'O ā mai \'oe'
+  },
+  {
+    question: 'Fine, thank you',
+    answers: ['\'O ā mai \'oe.','Manuia, fa\'afetai.', 'Fa\'amolemole', 'Fa\'amālūlū atu'],
+    correctAnswer: 'Manuia, fa\'afetai.'
+  },
+  {
+    question: 'What is your name?',
+    answers: ['\'O ā mai \'oe.','\'O ai lou suafa?', 'Tōfā soifua', 'Fa\'amālūlū atu'],
+    correctAnswer: '\'O ai lou suafa?'
+  },
+  {
+    question: 'I\'m sorry',
+    answers: ['Fa\'amālūlū atu','Fa\'afetai fo\'i.', 'O ā mai \'oe', 'Manuia le taeao'],
+    correctAnswer: 'Fa\'amālūlū atu'
+  },
+  {
+    question: 'Thank you',
+    answers: ['\'O ā mai \'oe.','Fa\'afetai.', 'Tōfā soifua', 'Fa\'amālūlū atu'],
+    correctAnswer: 'Fa\'afetai.'
+  },
+  {
+    question: 'I don\'t know',
+    answers: ['\'O ā mai \'oe.','Ou te leiloa', 'Manuia le po', 'Ia manuia'],
+    correctAnswer: 'Ou te leiloa'
+  },
+  {
+    question: 'Where is the toilet?',
+    answers: ['\'O ā mai \'oe.','Fa\'afetai.', '\'Ou te lē mālamalama.', 'O fea le faleuila?'],
+    correctAnswer: 'O fea le faleuila?'
+  },
+  {
+    question: 'Call the police!',
+    answers: ['Fia ola!','Vili le leoleo!', '\'Ou te lē mālamalama.', 'Tulou lava'],
+    correctAnswer: 'Vili le leoleo!'
   }
-
 ];
 
 module.exports = questions;
