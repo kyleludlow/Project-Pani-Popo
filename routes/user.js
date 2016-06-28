3a029df44b09eee51c9161513d6f569dd53e16a8
cjjujubee <connie.jew@gmail.com>
2016-06-28 08:24:15 -0700

    Adds phrases document to be seeded into database

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var User = require('../services/user');
 
+//gets all users
+
 router.get('/users', function(req, res) {
   User.list(function(users) {
     res.json(users);
@@ -9,6 +11,8 @@ router.get('/users', function(req, res) {
     res.status(400).json(err);
   });
 });
+
+//create users at registration endpoint
 
 router.post('/register', function(req, res) {
   User.save(new User({
