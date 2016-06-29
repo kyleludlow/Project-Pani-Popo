f082ab3425697fe136d0ac3835e15af1cac1e771
cjjujubee <connie.jew@gmail.com>
2016-06-28 22:52:49 -0700

    Adds ability to find single question

--- routes/question.js
+++ routes/question.js
@@ -10,12 +10,20 @@ router.get('/questions', function(req, res) {
     });
 });
 
-router.post('/questions', function(req, res) {
-    Question.save(req.body.name, function(question) {
-        res.status(201).json(question);
-    }, function(err) {
-        res.status(400).json(err);
-    });
+router.get('/questions/:question', function(req, res) {
+  Question.findOne(req.params.question, function(question) {
+    res.json(question);
+  }, function(err) {
+      res.status(400).json(err);
+  });
 });
+
+// router.post('/questions', function(req, res) {
+//     Question.save(req.body.name, function(question) {
+//         res.status(201).json(question);
+//     }, function(err) {
+//         res.status(400).json(err);
+//     });
+// });
 
 module.exports = router;
